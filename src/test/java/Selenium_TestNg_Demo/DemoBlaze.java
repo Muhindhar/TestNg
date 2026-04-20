899c4d8041d99d09334a4a3566e83eb9fcf39ba0
Muhindhar <muhindharsv27@gmail.com>
2026-04-20 23:33:25 +0530

    Update DemoBlaze.java

--- src/test/java/Selenium_TestNg_Demo/DemoBlaze.java
+++ src/test/java/Selenium_TestNg_Demo/DemoBlaze.java
@@ -1,9 +1,7 @@
 package Selenium_TestNg_Demo;
 
 import org.testng.annotations.Test;
-
-import junit.framework.Assert;
-
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
@@ -13,85 +11,67 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.NoAlertPresentException;
 
 import java.time.Duration;
 
 public class DemoBlaze {
+
     WebDriver driver;
     WebDriverWait wait;
 
     @BeforeMethod
     public void beforeTest() {
-        String browser = "chrome";  
-        String url = "https://www.demoblaze.com";
-        if (browser.equalsIgnoreCase("firefox")) {
-            driver = new FirefoxDriver();
-        } else {
-            driver = new ChromeDriver();
-        }
-
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        driver.get(url);
+        driver.get("https://www.demoblaze.com");
     }
 
     @DataProvider(name = "logindata")
     public Object[][] getData() {
         return new Object[][]{
-                {"muhi", "admin"}, 
-                {"admin","233"},
-                {"admin","admin"}
-               };
-    		}
+                {"muhi", "admin"},
+                {"admin", "233"},
+                {"admin", "admin"}
+        };
+    }
+
     @Test(dataProvider = "logindata")
-    public void loginTest(String username, String password) {
+    public void loginTest(String username, String password) throws InterruptedException {
+
         driver.findElement(By.id("login2")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginusername")))
-                .sendKeys(username);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginusername"))).sendKeys(username);
         driver.findElement(By.id("loginpassword")).sendKeys(password);
         driver.findElement(By.xpath("//button[text()='Log in']")).click();
-        WebElement check = driver.findElement(By.xpath("//a[@id='cat']"));   
-        wait.until(ExpectedConditions.alertIsPresent());
-       	Alert alert = driver.switchTo().alert();
-       	alert.accept();    
-       	if(check.isDisplayed()) {
-       		System.out.println("Success");
-       	}
-       	String a = check.getText();
-       	Assert.assertEquals(check, a);
+
+        if (isAlertPresent()) {
+            Alert alert = driver.switchTo().alert();
+            String msg = alert.getText();
+            alert.accept();
+            System.out.println("Login Failed: " + msg);
+            Assert.assertTrue(msg.contains("Wrong"));
+        } else {
+            WebElement user = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nameofuser")));
+            String text = user.getText();
+            System.out.println("Login Success: " + text);
+            Assert.assertTrue(text.contains("Welcome"));
+        }
     }
-    @Test
-    public void Invalid1() {
-  	  driver.findElement(By.xpath("//a[@id='login2']")).click();
-  	  driver.findElement(By.xpath("//input[@id='loginusername']")).sendKeys("muhi");
-  	  driver.findElement(By.xpath("//input[@id='loginpassword']")).sendKeys("muhi112");
-  	  driver.findElement(By.xpath("//button[text()='Log in']")). click();
-  	  wait.until(ExpectedConditions.alertIsPresent());
-  	  Alert alert = driver.switchTo().alert();
-  	  String al = alert.getText();
-  	  String de = "Wrong password.";
-  	  alert.accept(); 
-  	  Assert.assertEquals(al,de);
-  	  System.out.println("Wrong password");
+
+    public boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
-    @Test
-    public void Invalid2() {
-  	  driver.findElement(By.xpath("//a[@id='login2']")).click();
-  	  driver.findElement(By.xpath("//input[@id='loginusername']")).sendKeys("muhi");
-  	  driver.findElement(By.xpath("//input[@id='loginpassword']")).sendKeys("admin");
-  	  driver.findElement(By.xpath("//button[text()='Log in']")). click();
-  	  wait.until(ExpectedConditions.alertIsPresent());
-  	  Alert alert = driver.switchTo().alert();
-  	  String al = alert.getText();
-  	  String de = "Wrong password.";
-  	  alert.accept();
-  	  Assert.assertEquals(al,de);
-  	  System.out.println("Wrong Username.");
-    }
+
     @AfterMethod
     public void afterTest() {
         driver.quit();
